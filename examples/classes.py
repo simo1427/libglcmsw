bd2c54ce855546e338ecdf9fa815f9ff04a523f6
simo1427 <simeon.a.atanasov@gmail.com>
2020-10-31 16:51:33 +0200

    updated classes.py to import only libglcmsw.SlidingWindow

--- examples/classes.py
+++ examples/classes.py
@@ -1,10 +1,10 @@
 if __name__ == "__main__":
-    import libglcmsw
+    from libglcmsw import SlidingWindow
 
-    whole=libglcmsw.SlidingWindow("./input.tif",13,"homogeneity",downscale=1)
+    whole=SlidingWindow("./input.tif",13,"homogeneity",downscale=1)
     whole.render("tiling",tilesz=125)
-    whole.savetif("outputclassraster", type="uint8")
+    whole.savetif("outputclasstiling", type="uint8")
 
-    downscale=libglcmsw.SlidingWindow("./input.tif",13,"homogeneity",downscale=2)
+    downscale=SlidingWindow("./input.tif",13,"homogeneity",downscale=2)
     downscale.render("raster",rowssave=80)
     downscale.savetif("outputclassraster", type="uint8")
