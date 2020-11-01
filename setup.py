6f5d1b7721fd65a93ff86d3fba5424382f595fe2
simo1427 <simeon.a.atanasov@gmail.com>
2020-11-01 19:54:17 +0200

    updated LICENSE, README.md and dist archives

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     author_email="simeon.a.atanasov@gmail.com",
     despcription="A module that is able to produce sliding window image of large files",
     long_description=long_description,
-    long_descroption_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url="https://github.com/simo1427/libglcmsw",
     packages=setuptools.find_packages(),
     classifiers=[
