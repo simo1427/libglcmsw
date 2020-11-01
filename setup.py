8f2dfa46a1e7c32a4cf1e5fe818df128d6549bc9
simo1427 <simeon.a.atanasov@gmail.com>
2020-11-01 19:09:26 +0200

    Updated setup.py classifiers and binaries respectively

--- setup.py
+++ setup.py
@@ -11,12 +11,15 @@ setuptools.setup(
     despcription="A module that is able to produce sliding window image of large files",
     long_description=long_description,
     long_descroption_content_type="text/markdown",
-    url="",
+    url="https://github.com/simo1427/libglcmsw",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: - :: -",
-        "Operating System :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Image Processing"
     ],
     python_requires='>3.7',
     install_requires=['scikit-image','openslide-python']
