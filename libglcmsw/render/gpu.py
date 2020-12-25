473eb15ecebdb9e0470bdbba421ed8e67b201cf3
simo1427 <simeon.a.atanasov@gmail.com>
2020-12-25 14:47:31 +0200

    added raster rendering

--- libglcmsw/render/gpu.py
+++ libglcmsw/render/gpu.py
@@ -6,6 +6,7 @@ from skimage.util import img_as_ubyte
 from skimage.color import rgb2gray
 import itertools
 import time
+from ..io import openimg
 
 def singletilecpu(im, windowsz, prop,angle, dist,bitdepth, target):
   if target=="cuda":
@@ -46,33 +47,48 @@ func tilerenderlist
 
 
 def tilerenderlist(dpath, inptile, windowsz, **kwargs):
-  workers = kwargs.get("ncores", multiprocessing.cpu_count() // 2 - 1)
-  if multiprocessing.cpu_count() < workers or workers < 0:
-    raise ValueError("Invalid number of workers")
-  prop = kwargs.get("prop", "homogeneity")
-  if len(inptile) < workers and len(inptile):
-    workers = len(inptile)
+    workers = kwargs.get("ncores", multiprocessing.cpu_count() // 2 - 1)
+    if multiprocessing.cpu_count() < workers or workers < 0:
+        raise ValueError("Invalid number of workers")
+    prop = kwargs.get("prop", "homogeneity")
+    if len(inptile) < workers and len(inptile):
+        workers = len(inptile)
 
-  angle = kwargs.get("angle", 0)
-  distance = kwargs.get("distance", 1)
-  bitdepth = kwargs.get("bitdepth", 256)
-  target=kwargs.get("target", "cuda")
-  print(f"Using GPU for rendering")
-  tiles=[]
-  for tile in inptile:
-    ni, nj = tile
-    tiles.append(img_as_ubyte(rgb2gray(np.load(dpath + f"/{ni}_{nj}.npy"))))
-  begintotal = time.perf_counter()
-  results = map(singletilecpu, tiles, itertools.repeat(windowsz),
-                         itertools.repeat(prop), itertools.repeat(angle), itertools.repeat(distance), itertools.repeat(bitdepth), itertools.repeat(target))
-  for p in inptile:
-    try:
-      ni, nj = p
-      np.save(dpath + f"/g{ni}_{nj}.npy", np.ascontiguousarray(next(results)), allow_pickle=True)
-      print(p)
-    except StopIteration:
-      break
+    angle = kwargs.get("angle", 0)
+    distance = kwargs.get("distance", 1)
+    bitdepth = kwargs.get("bitdepth", 256)
+    target=kwargs.get("target", "cuda")
+    print(f"Using GPU for rendering")
+    tiles=[]
+    for tile in inptile:
+        ni, nj = tile
+        tiles.append(img_as_ubyte(rgb2gray(np.load(dpath + f"/{ni}_{nj}.npy"))))
+    begintotal = time.perf_counter()
+    results = map(singletilecpu, tiles, itertools.repeat(windowsz),
+                           itertools.repeat(prop), itertools.repeat(angle), itertools.repeat(distance), itertools.repeat(bitdepth), itertools.repeat(target))
+    for p in inptile:
+        try:
+          ni, nj = p
+          np.save(dpath + f"/g{ni}_{nj}.npy", np.ascontiguousarray(next(results)), allow_pickle=True)
+          print(p)
+        except StopIteration:
+          break
 
-  finishtotal = time.perf_counter()
-  print(f'Ended in {round(finishtotal - begintotal, 3)}')
+    finishtotal = time.perf_counter()
+    print(f'Ended in {round(finishtotal - begintotal, 3)}')
 
+
+def rasterrender(osobj,windowsz,**kwargs):
+    prop = kwargs.get("prop", "homogeneity")
+    dsfact=kwargs.get("downscale", 1)
+    angle = kwargs.get("angle", 0)
+    distance = kwargs.get("distance", 1)
+    bitdepth = kwargs.get("bitdepth", 256)
+    target=kwargs.get("target", "cuda")
+    print(f"Using GPU for rendering")
+    im = img_as_ubyte(rgb2gray(openimg.tonpyarr(osobj, downscale=dsfact)))
+    begintotal = time.perf_counter()
+    glcm_hom=singletilecpu(im, windowsz,prop, angle, distance, bitdepth, target)
+    finishtotal = time.perf_counter()
+    print(f'Ended in {round(finishtotal - begintotal, 3)}')
+    return glcm_hom
